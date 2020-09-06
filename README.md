0d7411d8165b4037d68fcf21f567e55eaf890803
MustafaMadkour <madkour.dev@gmail.com>
2020-09-06 11:53:19 +0200

    add README file - add section 4

---
+++ README.md
@@ -0,0 +1,13 @@
+# Title
+Udacity Landing Page Project
+
+## Description
+Converting Static web landing page to a Dynamic one 
+
+## Stack
+HTML - CSS - JavaScript
+
+## Functionality
+- Navigation Bar
+- Dynamic Active State
+- Scrolling effect
---
+++ js/app.js
@@ -0,0 +1,12 @@
+const sections = document.querySelectorAll('section');
+
+
+function createNavItem() {
+    const aLink = document.createElement('a');
+    const liLink = document.createElement('li');
+    const linkTitle = document.                                                                                             
+}
+
+for (let i = 0; i <= sections.length; i++) {
+    createNavItem();
+}
