fe9c4ee16c6ad182bc385be4d2c6ad2447effb86
MustafaMadkour <madkour.dev@gmail.com>
2020-09-07 16:12:57 +0200

    finalizing

--- js/app.js
+++ js/app.js
@@ -57,10 +57,48 @@ navBuilder();
 
 // Add class 'active' to section when near top of viewport
 
+// largest value leess or equal to the number
+const offset = (section) => {
+    return Math.floor(section.getBoundingClientRect().top);
+};
+
+// remove active class
+const removeActive = (section) => {
+    section.classList.remove('your-active-class');
+};
+
+// add active class
+const addActive = (cond, section) => {
+    cond ? section.classList.add('your-active-class') : null;
+};
+
+// Active Section
+const activeSection = () => {
+    for (section of sections) {
+        const secOffset = offset(section);
+
+        inView = () => (secOffset < 150 && secOffset >= -150);
+
+        removeActive(section);
+        addActive(inView(), section);
+    };
+};
+
+window.addEventListener('scroll', activeSection());
 
 // Scroll to anchor ID using scrollTO event
+const scrolling = () => {
+    const links = document.querySelectorAll('.navbar__menu a');
+    for (link of links) {
+        link.addEventListener('click', () => {
+            for (section in sections) {
+                section.addEventListener('scroll', sectionScroll(link));
+            };
+        });
+    };
+};
 
-
+scrolling();
 /**
  * End Main Functions
  * Begin Events
